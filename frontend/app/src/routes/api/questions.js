01d121c609f2f0d2969d866d16576291ede62bfb
Thomas <thomas.petersson@nurofy.no>
2019-10-04 16:08:08 +0200

    stuff

--- frontend/app/src/routes/api/questions.js
+++ frontend/app/src/routes/api/questions.js
@@ -2,15 +2,20 @@ import fetch from "node-fetch";
 
 export async function get(req, res) {
     try {
-        const url = `${process.env.API_URL}api/v1/questions?multichoice=6&dropdown=6`;
-
+        const url = `${process.env.API_URL}/api/v1/questions?limit=10`;
+        console.log(url);
+        
         const resp = await fetch(url);
+        console.log(resp);
+        
         const json = await resp.json();
-
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify(json));
+        console.log(json);
+        
+        res.json(json);
 
     } catch (error) {
+        console.log(error);
+        
         res.statusCode = 500;
         res.end({ error });
     }
