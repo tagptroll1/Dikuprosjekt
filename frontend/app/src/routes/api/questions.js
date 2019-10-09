730c09255306e051f341450664dd52e090900484
Thomas <thomas.petersson@nurofy.no>
2019-10-09 13:10:13 +0200

    fix: fixed?

--- frontend/app/src/routes/api/questions.js
+++ frontend/app/src/routes/api/questions.js
@@ -1,21 +1,12 @@
-import fetch from "node-fetch";
+import axios from "axios";
 
 export async function get(req, res) {
     try {
         const url = `${process.env.API_URL}/api/v1/questions?limit=10`;
-        console.log(url);
-        
-        const resp = await fetch(url);
-        console.log(resp);
-        
-        const json = await resp.json();
-        console.log(json);
-        
-        res.json(json);
-
+        const resp = await axios.get(url);
+        res.json(resp.data);
     } catch (error) {
         console.log(error);
-        
         res.statusCode = 500;
         res.end({ error });
     }
