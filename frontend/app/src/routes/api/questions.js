4397a23d37c3e990d5cf892255f7b6bf6a042cbb
Thomas <thomas.petersson@nurofy.no>
2019-10-12 16:59:30 +0200

    feat: create spm api

--- frontend/app/src/routes/api/questions.js
+++ frontend/app/src/routes/api/questions.js
@@ -13,3 +13,36 @@ export async function get(req, res) {
         res.status(500).end(JSON.stringify({ error }));
     }
 }
+
+export async function post(req, res) {
+    const { body } = req;
+
+    const url = `${process.env.API_URL}/api/v1/questions`;
+
+    try {
+        const resp = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `token ${process.env.API_KEY}`,
+            },
+            body: JSON.stringify(body),
+        });
+
+        const text = await resp.text();
+
+        try {
+            const json = JSON.parse(text);
+
+            if (resp.ok) {
+                res.status(201).json(json)
+            } else {
+                res.status(resp.status).json(json);
+            }
+        } catch (error) {
+            res.status(500).end("Json parsing failed, text: " + text);
+        }
+    } catch (error) {
+        res.status(500).end("Request failed")
+    }
+}
