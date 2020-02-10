195bfee95a18d382a74fba983ddcf098b151856e
Joachim Morken <joknumo@gmail.com>
2020-02-10 13:02:36 +0100

    Added server logging

--- frontend/app/src/server.js
+++ frontend/app/src/server.js
@@ -6,6 +6,11 @@ import bodyParser from "body-parser";
 import sessionFileStore from "session-file-store";
 import * as sapper from "@sapper/server";
 
+import * as expressWinston from "express-winston";
+import * as sapper from "@sapper/server";
+import * as winston from "winston";
+
+
 const FileStore = sessionFileStore(session);
 
 const { PORT, NODE_ENV } = process.env;
@@ -13,6 +18,8 @@ const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
 const app = express();
+
+const ignoredPaths = ["/auth/callback", "/favicon.png", "/global.css", "/service-worker.js"];
 
 
 const sess = {
@@ -42,3 +49,51 @@ app.use(bodyParser.json())
     .listen(PORT, err => {
         if (err) console.log("error", err);
     });
+
+// Setup loggers
+winston.loggers.add("file-logger", {
+    level: "info",
+    format: winston.format.json(),
+    defaultMeta: { service: "user-service" },
+    meta: true,
+    transports: [
+        new winston.transports.File({ filename: "error.log", level: "error" }),
+        new winston.transports.File({ filename: "meta.log" }),
+    ],
+});
+
+if (dev) {
+    winston.loggers.add("dev-logger", {
+        format: winston.format.simple(),
+        transports: [
+            new winston.transports.Console({
+                level: "debug",
+            }),
+        ],
+    });
+}
+
+app
+    .use(
+        expressWinston.logger({
+            transports: [new winston.transports.Console()],
+            format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+            meta: false,
+            msg: "HTTP {{res.statusCode}} {{req.method}} {{req.url}} {{res.responseTime}}ms",
+            expressFormat: false,
+            colorize: true,
+            ignoreRoute: ({ path }) => ignoredPaths.includes(path) || path.slice(0, 7) === "/client",
+        })
+    )
+    .use(
+        expressWinston.logger({
+            level: "info",
+            format: winston.format.json(),
+            defaultMeta: { service: "user-service" },
+            transports: [
+                new winston.transports.File({ filename: "error.log", level: "error" }),
+                new winston.transports.File({ filename: "meta.log" }),
+            ],
+            ignoreRoute: ({ path }) => ignoredPaths.includes(path) || path.slice(0, 7) === "/client",
+        })
+    )
