5acdacb70391ca16967c960927f8adac3e2eddd3
Joachim Morken <joknumo@gmail.com>
2020-03-15 12:33:36 +0100

    Kap 8

--- frontend/app/src/routes/question/index.svelte
+++ frontend/app/src/routes/question/index.svelte
@@ -9,7 +9,7 @@
     let count = 0;
     while ($questions.length <= 0 && process.browser && count < 10) {
       try {
-        const resp = await fetch("api/questions?tags=kap7"); // Change this to get different questions
+        const resp = await fetch("api/questions?tags=kap8"); // Change this to get different questions
         if (!resp.ok) {
           continue;
         }
